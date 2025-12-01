757a22dbccff02350c28d2eb2004762a9c3248ca
NEER4J <ittsneeraj@gmail.com>
2025-12-01 19:58:53 +0530

    mon

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,8 +29,7 @@ export default function RootLayout({
       <body className={`${geistSans.className} antialiased`}>
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
-          enableSystem
+          defaultTheme="light"
           disableTransitionOnChange
         >
           {children}
